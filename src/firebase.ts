afffd03088f72afbe10d83b679299e72857811bf
esnya <esnya@nekometer.com>
2019-09-13 19:50:56 +0900

    :recycle: Separate key

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,18 +1,11 @@
 import firebase from 'firebase/app';
+import config from './firebase.key.json';
 import 'firebase/auth';
 import 'firebase/firestore';
 
 export default firebase;
 
-export const app = firebase.initializeApp({
-  apiKey: 'AIzaSyARB2SconW0FD_tIwhCSjSKUAZilzr5PLc',
-  authDomain: 'huginn-8f1e6.firebaseapp.com',
-  databaseURL: 'https://huginn-8f1e6.firebaseio.com',
-  projectId: 'huginn-8f1e6',
-  storageBucket: 'huginn-8f1e6.appspot.com',
-  messagingSenderId: '256390309290',
-  appId: '1:256390309290:web:2a9c861cbe63923b',
-});
+export const app = firebase.initializeApp(config);
 
 export const auth = app.auth();
 export const firestore = app.firestore();
